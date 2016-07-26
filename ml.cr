e825509d7757212503df8faa3a83c3f1956cdb21
Pablo Brusco <pablo.brusco@gmail.com>
2016-07-26 01:14:17 -0300

    ml load csv

--- ml.cr
+++ ml.cr
@@ -8,4 +8,17 @@ module ML
     right = actual.zip(predicted).map {|(x,y)| x == y ? 1 : 0}.sum
     right.to_f / actual.size
   end
+
+  def ML.load_csv(csv_file)
+    xs = [] of Array(Float32)
+    ys = [] of String
+
+    f = File.open(csv_file)
+    CSV.parse(f, separator: ',').each_with_index do |row, idx|
+      next if idx == 0
+      xs << row[0, row.size - 1].map {|x| x.to_f32}
+      ys << row[row.size - 1]
+    end
+    {xs, ys}
+  end
 end
--- test.cr
+++ test.cr
@@ -3,31 +3,19 @@ require "./knn"
 require "./array"
 require "./ml"
 
-f = File.open("iris.csv")
+x, y = ML.load_csv("iris.csv")
 
-alias InstancesType = Float32
-alias LabelsType = String
+train_index, test_index = ML.train_test_split(x.size, percentage=0.9)
 
-X = [] of Array(Float32)
-y = [] of String
-
-CSV.parse(f, separator: ',').each_with_index do |row, idx|
-  next if idx == 0
-  X << row[0, 4].map {|x| x.to_f32}
-  y << row[4]
-end
-
-train_index, test_index = ML.train_test_split(X.size, percentage=0.9)
-
-x_train, y_train = X[train_index], y[train_index]
-x_test, y_test = X[test_index], y[test_index]
+x_train, y_train = x[train_index], y[train_index]
+x_test, y_test = x[test_index], y[test_index]
 
 puts "Set sizes: x_train #{x_train.size} y_train #{y_train.size} x_test #{x_test.size} y_test #{y_test.size}"
 
 
 
 (5..150).step(10).each do |n|
-  clf = KNeighborsClassifier(typeof(X.first), typeof(y.first)).new(n_neighbors=n)
+  clf = KNeighborsClassifier(typeof(x.first), typeof(y.first)).new(n_neighbors=n)
   clf.fit(x_train, y_train)
   y_pred = clf.predict(x_test)
   acc = ML.accuracy(y_test, y_pred)
